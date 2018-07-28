f76813c64c0536304c870b742bc09363d2845acc
tk <torben.koenke@gmail.com>
2018-07-29 00:42:29 +0200

    NuGet stuff

--- NuGet.Publish.bat
+++ NuGet.Publish.bat
@@ -1,4 +1,6 @@
 @echo off
+set /p last=<last_version
+@echo Last published version is %last%
 set /p version="Version (ex. 4.5.0): "
 nuget.exe pack Krypton.Components.Suite.nuspec -Version %version%
 nuget.exe push Krypton.Components.Suite.%version%.nupkg
